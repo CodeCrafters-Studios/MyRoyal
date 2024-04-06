1f657129f371c8c0a4bc3b1be06170a5109aff9c
Alghany Kennedy <alghanyka@gmail.com>
2024-04-06 08:49:07 +0700

    feat: create base/config include app_config, app_constants, environment_config

---
+++ lib/base/config/app_constants.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+const String appTitle = 'I-ROYAL';
+const Size designSize = Size(375, 812);
+const emptyBox = SizedBox();
+
+const String CACHE_ACCESS_TOKEN = 'access_token';
+const String CACHE_REFRESH_TOKEN = 'refresh_token';
+const String CACHE_FCM_TOKEN = 'fcm_token';
+const String CACHE_ACCESS_USER_EMAIL = 'user_email';
+const String CACHE_ACCESS_USER_NAME = 'user_name';
+const String CACHE_ACCESS_USER_PASSWORD = 'user_password';
+const String CACHE_ACCESS_PHONE_NUMBER = 'phone_number';
+const String CACHE_ACCESS_PHOTO = 'photo';
+const String CACHE_ACCESS_USER_ADDRESS = 'user_address';
+const String CACHE_ACCESS_ACCOUNT_NO = 'account_no';
+const String CACHE_API_LOG = 'log_api';
+const String CACHE_LOGIN_RESPONSE = 'login-response';
+const String CACHE_USER_LOGIN = 'user-login';
+const String CACHE_FIRST_TIME_OPEN_APP = 'first-time-open-app';
+const String CLOSE_LOG = 'closelog';
+const String APP_LANGUAGE = 'app_language';
+const IROYAL_STORAGE = 'iroyal_secure_storage';
+const CACHE_MENU = 'cache-menu';
+const CACHE_FAVORITES = 'cache-favorites';
+const CACHE_ONBOARDING_STEP = 'cache-onboarding-step';
+const CACHE_LANGUAGE = 'cache-language';
---
+++ lib/base/config/environment_config.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+enum EnvironmentType { development, staging, production }
+
+class EnvironmentConfig {
+  const EnvironmentConfig.development()
+      : environment = EnvironmentType.development,
+        androidBaseUrl = '',
+        iosBaseUrl = '';
+
+  const EnvironmentConfig.staging()
+      : environment = EnvironmentType.staging,
+        androidBaseUrl = '',
+        iosBaseUrl = '';
+
+  const EnvironmentConfig.production()
+      : environment = EnvironmentType.production,
+        androidBaseUrl = '',
+        iosBaseUrl = '';
+
+  final EnvironmentType environment;
+  final String androidBaseUrl;
+  final String iosBaseUrl;
+
+  static late EnvironmentType environmentType;
+
+  String get baseUrl => Platform.isIOS ? iosBaseUrl : androidBaseUrl;
+}
