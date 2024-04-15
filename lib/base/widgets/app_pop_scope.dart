b1fba6d20dea960f9be21754b05d331ff48cc578
Alghany Kennedy Adam <alghany.adam@royalcorp.id>
2024-04-15 09:59:47 +0700

    feat: splash screen

---
+++ lib/base/widgets/app_pop_scope.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppPopScope extends StatelessWidget {
+  const AppPopScope({super.key, required this.child, this.onBack});
+  final Widget child;
+  final Function()? onBack;
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      child: child,
+      onWillPop: () async {
+        if (onBack != null) {
+          onBack!.call();
+        }
+        return false;
+      },
+    );
+  }
+}
