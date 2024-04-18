223334e1c291abcbb1d67be9fbb8a92a34521eab
Alghany Kennedy Adam <alghany.adam@royalcorp.id>
2024-04-18 08:37:19 +0700

    feat: page profile virgin

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -6,6 +6,8 @@ import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/login/presentation/bindings/login_binding.dart';
 import '../modules/login/presentation/views/login_view.dart';
+import '../modules/profile/bindings/profile_binding.dart';
+import '../modules/profile/views/profile_view.dart';
 import '../modules/splash/bindings/splash_binding.dart';
 import '../modules/splash/views/splash_view.dart';
 
@@ -38,6 +40,11 @@ class AppPages {
       name: _Paths.BOTTOMNAVBAR,
       page: () => const BottomnavbarView(),
       binding: BottomnavbarBinding(),
+    ),
+    GetPage(
+      name: _Paths.PROFILE,
+      page: () => const ProfileView(),
+      binding: ProfileBinding(),
     ),
   ];
 }
