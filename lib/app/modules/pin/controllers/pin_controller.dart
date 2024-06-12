8390e3ae5823b8bf78f6c5b01339da6fe5da08e6
Alghany Kennedy Adam <alghany.adam@royalcorp.id>
2024-06-12 08:51:40 +0700

    feat: add security pin for feature payroll

--- lib/app/modules/pin/controllers/pin_controller.dart
+++ lib/app/modules/pin/controllers/pin_controller.dart
@@ -1,23 +1,38 @@
+import 'dart:ui';
+
 import 'package:get/get.dart';
+import 'package:iroyal/app/routes/app_pages.dart';
+import 'package:iroyal/base/design/colors.dart';
 
 class PinController extends GetxController {
-  //TODO: Implement PinController
+  var pin = ''.obs;
+  var outlineColor = Rx<Color>(grey);
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  void setPin(String value) {
+    if (pin.value.length < 6) {
+      outlineColor.value = grey;
+      pin.value += value;
+    }
+    // Check if the pin is '000000'
+    if (pin.value == '000000' && pin.value.length == 6) {
+      outlineColor.value = grey;
+      Get.offAndToNamed(Routes.PAYROLL);
+    } else if (pin.value.length == 6) {
+      pin.value = '';
+      outlineColor.value = red;
+    }
   }
 
-  @override
-  void onReady() {
-    super.onReady();
+  void onKeyDelete() {
+    if (pin.value.isNotEmpty) {
+      pin.value = pin.value.substring(0, pin.value.length - 1);
+    }
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  void forgotPin() {
+    // Handle forgot pin logic
   }
 
-  void increment() => count.value++;
+  List<String> get keyboards =>
+      ['1', '2', '3', '4', '5', '6', '7', '8', '9', '', '0', 'back'];
 }
---
+++ lib/app/modules/pin/views/components/pin_text_view.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:iroyal/app/modules/pin/controllers/pin_controller.dart';
+import 'package:iroyal/base/design/styles.dart';
+import 'package:iroyal/base/widgets/card_app.dart';
+
+class PinTextView extends StatelessWidget {
+  const PinTextView({
+    super.key,
+    required this.pin,
+    required this.controller,
+  });
+  final String pin;
+  final PinController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    final lengthNotDot = 6 - pin.length;
+    final safeLengthNotDot = lengthNotDot < 0 ? 0 : lengthNotDot;
+    final emptyCards = List<Widget>.generate(
+        safeLengthNotDot,
+        (index) => PinHolder(
+              controller: controller,
+            ));
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ...pin.split('').map(
+              (e) => PinHolder(
+                controller: controller,
+                isShowDot: true,
+              ),
+            ),
+        ...emptyCards,
+      ],
+    );
+  }
+}
+
+class PinHolder extends StatelessWidget {
+  const PinHolder({
+    super.key,
+    this.isShowDot = false,
+    required this.controller,
+  });
+  final bool isShowDot;
+  final PinController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => CardApp(
+        outlineColor: controller.outlineColor.value,
+        isOutlined: true,
+        borderWidth: 1,
+        margin: REdgeInsets.symmetric(horizontal: 4),
+        width: 44.w,
+        height: 44.w,
+        child: isShowDot
+            ? Center(
+                child: Text(
+                  '●',
+                  style: TS.titleMedium,
+                ),
+              )
+            : const SizedBox(),
+      ),
+    );
+  }
+}
